c3ee7d9ac8ec45cf8e9fb40fa3d7a8dbcac9f625
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-05-23 13:57:40 -0500

    Completed all calculator buttons

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
+// initializes the four math buttons & output node
 const addButton = document.querySelector("#addButton");
 const subtractButton = document.querySelector("#subtractButton");
 const multiplyButton = document.querySelector("#multiplyButton");
 const divideButton = document.querySelector("#divideButton");
+let output = document.querySelector("#output");
 
-addButton.addEventListener("click", add());
-subtractButton.addEventListener("click", subtract() );
-multiplyButton.addEventListener("click", multiply() );
-divideButton.addEventListener("click", divide() );
+// takes an id name & extracts the number from that element
+getNum = (id) => {
+  num = document.querySelector(`#${id}`).value;
+  num = parseFloat(num);
+  return num;
+}
 
 add = () => {
-  let num1 = document.querySelector("#num1").value;
-  let num2 = document.querySelector("#num2").value;
-  console.log(num1 + num2);
-};
+  let num1 = getNum("num1");
+  let num2 = getNum("num2");
+  output.textContent = (num1 + num2);
+}
+subtract = () => {
+  let num1 = getNum("num1");
+  let num2 = getNum("num2");
+  output.textContent = (num1 - num2);
+}
+multiply = () => {
+  let num1 = getNum("num1");
+  let num2 = getNum("num2");
+  output.textContent = (num1 * num2);
+}
+divide = () => {
+  let num1 = getNum("num1");
+  let num2 = getNum("num2");
+  output.textContent = (num1 / num2);
+}
+
+addButton.addEventListener("click", add);
+subtractButton.addEventListener("click", subtract);
+multiplyButton.addEventListener("click", multiply);
+divideButton.addEventListener("click", divide);
